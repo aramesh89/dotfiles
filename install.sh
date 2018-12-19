d7aa686a4a734a669012cd020ae6493ac75e7232
aramesh89 <ashishramesh890@gmail.com>
2018-12-18 19:27:39 -0800

    added .emacs.d

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 #Dotfile install script
+
+#vars
+dotsdir=~/dotfiles
+olddir=~/dotfiles_old
+files=".vimrc .emacs.d"
+
+#Removing old dotfiles then symlink new dots
+echo "Creating $olddir for backup of existing dots"
+mkdir -p $olddir
+echo "Changing into $dotsdir"
+cd $dotsdir
+for file in $files; do
+	echo "Moving existing dots into $olddir"
+	mv ~/$file ~/dotfiles_old/
+	echo "Creating symlink to $file in home directory"
+	ln -s $dotsdir/$file ~/$file
+done
